bf75186b613f5e64abdfc886387ffb6e7d77abb2
Patricia Felix <patriciafelixx@outlook.com>
2021-08-04 18:04:22 -0300

    Conexão com banco de dados

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 
+require('./database');
+
+app.use(express.json());
 app.use(routes);
 
 app.listen(3333);
