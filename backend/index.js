cd26813dce76e37687754889ce1c97d2c4151977
Patricia Felix <patriciafelixx@outlook.com>
2021-08-04 22:56:38 -0300

    Preparações para o consumo da api

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const routes = require('./routes');
 
 require('./database');
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
